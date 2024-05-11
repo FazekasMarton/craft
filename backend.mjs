877fd7047225ee8c2a28e1b21147a0b0ae601f57
FazekasMarton <fazekas.marton16@gmail.com>
2024-05-11 10:47:46 +0200

    first commit

---
+++ backend.mjs
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import bodyParser from "body-parser"
+
+//***************************************DEFINING SERVER *************************************************/
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const port = process.env.PORT || 6969;
+app.use(cors());
+
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
+app.use(bodyParser.json())
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+    console.log("Server is running on port", port);
+});
