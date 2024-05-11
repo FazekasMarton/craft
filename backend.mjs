20c883a8e557e270d54fdd896de51cf09cdf5234
FazekasMarton <fazekas.marton16@gmail.com>
2024-05-11 12:41:22 +0200

    riddle draw finished

--- backend.mjs
+++ backend.mjs
@@ -1,8 +1,11 @@
-import express from "express";
+import express, { json } from "express";
 import cors from "cors";
 import path from 'path';
 import { fileURLToPath } from 'url';
 import bodyParser from "body-parser"
+import http from 'http';
+import { Server } from 'socket.io';
+import fs from 'fs';
 
 //***************************************DEFINING SERVER *************************************************/
 
@@ -10,8 +13,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const port = process.env.PORT || 6969;
+
 app.use(cors());
+const port = process.env.PORT || 6969;
 
 app.use(
     bodyParser.urlencoded({
@@ -22,6 +26,42 @@ app.use(bodyParser.json())
 
 app.use(express.static('public'));
 
-app.listen(port, () => {
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+      origin: "http://localhost:5173",
+      methods: ["GET", "POST"]
+    }
+});
+
+let items = null
+let riddles = {}
+
+io.on('connection', async(socket) => {
+    await createRiddle(socket)
+
+    socket.on("disconnect", () => {
+        delete riddles[socket.id]
+    })
+});
+
+async function createRiddle(socket){
+    if(items == null){
+        items = JSON.parse(fs.readFileSync("./recipes.json", 'utf8')).data;
+    }
+    let riddle
+    let numberOfNulls = 0
+    do {
+        riddle = items[Math.floor(Math.random() * items.length)];
+        riddles[socket.id] = riddle
+        console.log(riddles[socket.id])
+        numberOfNulls = 0
+        riddles[socket.id].recipe.forEach(material => {
+            if(material == null) numberOfNulls++
+        });
+    } while (numberOfNulls >= 8);
+}
+
+server.listen(port, () => {
     console.log("Server is running on port", port);
 });
