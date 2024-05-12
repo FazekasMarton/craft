5cf73c03be0de6b5bc4b5bed3a8ab23a11f7e8d8
FazekasMarton <fazekas.marton16@gmail.com>
2024-05-12 17:39:57 +0200

    craftingtable ui finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import craftingTableArrow from './assets/craftingtablearrow.png'
 import io from 'socket.io-client';
 
 const socket = io('http://localhost:6969');
@@ -6,7 +7,32 @@ const socket = io('http://localhost:6969');
 function App() {
   return (
     <>
-      <div>szevasz</div>
+      <div id='craftingTable'>
+        <div id='craftingTitle'>Crafting</div>
+        <table>
+          <tbody>
+            <tr>
+              <td className='cragtingTableSlot' id='slot0'></td>
+              <td className='cragtingTableSlot' id='slot1'></td>
+              <td className='cragtingTableSlot' id='slot2'></td>
+            </tr>
+            <tr>
+              <td className='cragtingTableSlot' id='slot3'></td>
+              <td className='cragtingTableSlot' id='slot4'></td>
+              <td className='cragtingTableSlot' id='slot5'></td>
+            </tr>
+            <tr>
+              <td className='cragtingTableSlot' id='slot6'></td>
+              <td className='cragtingTableSlot' id='slot7'></td>
+              <td className='cragtingTableSlot' id='slot8'></td>
+            </tr>
+          </tbody>
+        </table>
+        <img id='craftingArrow' src={craftingTableArrow} alt="arrow"/>
+        <div id='item'/>
+      </div>
+      <div id='tips'></div>
+      <div id='items'></div>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import './App.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('wrapper')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
