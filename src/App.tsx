c5e225041b6a99bd0441f74e2dadea317ab24462
HorvathMate04 <culubpenguin@gmail.com>
2024-05-19 00:06:34 +0200

    image load fixed

--- src/App.tsx
+++ src/App.tsx
@@ -352,7 +352,7 @@ function App() {
               if (item.name.toLocaleLowerCase().includes(search.toLocaleLowerCase())) display = "flex"
               return (
                 <div key={`itemSlot#${index}`} className='itemSlot' style={{ display: display }}>
-                  <img src={"null"} alt={item.name} title={item.name} draggable onDrag={(e) => { setDropItem(e.currentTarget) }} />
+                  <img src={item.image} alt={item.name} title={item.name} draggable onDrag={(e) => { setDropItem(e.currentTarget) }} />
                 </div>
               )
             })}
